016790f0b78824248c15bda69469315027cbd21a
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-07 17:22:40 -0400

    Completed Author

--- Bookazon.Data/Author.cs
+++ Bookazon.Data/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,14 @@ namespace Bookazon.Data
 {
     public class Author
     {
+        [Key]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
 
     }
 }
