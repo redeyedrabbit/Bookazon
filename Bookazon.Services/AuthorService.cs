192b01ee8d3a7d9dc5327d79c8a7d562c2911b5c
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-08 15:45:39 -0400

    Added Identity Models as it did not merge correctly.

---
+++ Bookazon.Services/AuthorService.cs
@@ -0,0 +1,26 @@
+﻿using Bookazon.Data;
+using Bookazon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookazon.Services
+{
+    public class AuthorService
+    {
+        private readonly Guid _userId;
+
+        public AuthorService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateAuthor(AuthorCreate model)
+        {
+            var entity =
+                new Author()
+        }
+    }
+}
