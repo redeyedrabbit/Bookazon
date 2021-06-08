4ebeb40ddb026c4700daa309ba01772db0978b6b
Ben Ellis <Benjamin.Ellis7711@gmail.com>
2021-06-08 15:59:25 -0400

    Added Product and Authorship class. Added db sets to Identity Models

--- Bookazon.Data/Author.cs
+++ Bookazon.Data/Author.cs
@@ -15,8 +15,10 @@ namespace Bookazon.Data
         [Required]
         public string FirstName { get; set; }
 
-        //[Required]
-        //public string LastName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public virtual ICollection<Authorship> BooksAuthored { get; set; }
 
     }
 }
--- Bookazon.Data/IdentityModels.cs
+++ Bookazon.Data/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -24,6 +25,10 @@ namespace Bookazon.Data
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
         
         public static ApplicationDbContext Create()
         {
