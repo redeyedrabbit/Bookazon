107953ad01a105d28a3d171ad62175ea360f83c6
Rachel Bellwood <rbellwood@gmail.com>
2021-06-08 16:12:43 -0400

    Publisher Models updated

--- Bookazon.Models/Publisher/PublisherCreate.cs
+++ Bookazon.Models/Publisher/PublisherCreate.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bookazon.Models.Publisher
 {
-    class PublisherCreate
+    public class PublisherCreate
     {
+        [ForeignKey(nameof(Publisher))]
+        public int PublisherId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(30, ErrorMessage = "There are too many characters in this field.")]
+        public string Name { get; set; }
     }
 }
