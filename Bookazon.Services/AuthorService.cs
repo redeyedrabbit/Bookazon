8a61f4f59f49b2950fe961dc7e334a14bfa27abd
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-08 16:36:34 -0400

    Started AuthorService, need to add AuthorUpdate and AuthorDelete

--- Bookazon.Services/AuthorService.cs
+++ Bookazon.Services/AuthorService.cs
@@ -21,6 +21,98 @@ namespace Bookazon.Services
         {
             var entity =
                 new Author()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Authors.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
         }
+
+        public IEnumerable<AuthorListItem> GetAuthors()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Authors
+                    .Select(
+                        e =>
+                        new AuthorListItem
+                        {
+                            AuthorId = e.AuthorId,
+                            FirstName = e.FirstName,
+                            LastName = e.LastName
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public IEnumerable<AuthorDetail> GetAuthorByLastName(string lastName)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Authors
+                    .Where(e => e.LastName == lastName)
+                    .Select(
+                        e =>
+                        new AuthorDetail
+                        {
+                            AuthorId = e.AuthorId,
+                            FirstName = e.FirstName,
+                            LastName = e.LastName
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public IEnumerable<AuthorDetail> GetAuthorByFirstName(string firstName)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = 
+                    ctx
+                    .Authors
+                    .Where(e => e.FirstName == firstName)
+                    .Select(
+                        e =>
+                        new AuthorDetail
+                        {
+                            AuthorId = e.AuthorId,
+                            FirstName = e.FirstName,
+                            LastName = e.LastName
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public AuthorDetail GetAuthorByFullName(string firstName, string lastName)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Authors
+                    .Single(e => e.FirstName == firstName && e.LastName == lastName);
+                   return
+                    new AuthorDetail
+                    {
+                        AuthorId = entity.AuthorId,
+                        FirstName = entity.FirstName,
+                        LastName = entity.LastName
+                    };
+            }
+        }
+
+
     }
 }
