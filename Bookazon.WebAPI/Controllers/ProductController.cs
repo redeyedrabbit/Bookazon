c2131b011065638e24a6d8f3fcd69f041195c8bc
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-09 15:28:35 -0400

    Added an Authorize tag to Author, started ProductController

---
+++ Bookazon.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Bookazon.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bookazon.WebAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+
+        private ProductService CreateProductService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var productService = new ProductService(userId);
+            return productService;
+        }
+    }
+}
