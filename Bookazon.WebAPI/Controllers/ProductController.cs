22fc888bd2f15aa1bd3460d349e98273aedd604e
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-09 16:14:14 -0400

    Finished ProductController

--- Bookazon.WebAPI/Controllers/ProductController.cs
+++ Bookazon.WebAPI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Bookazon.Services;
+﻿using Bookazon.Models.Product;
+using Bookazon.Services;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,65 @@ namespace Bookazon.WebAPI.Controllers
 {
     public class ProductController : ApiController
     {
+        [Authorize]
+
+        public IHttpActionResult GetAll()
+        {
+            ProductService productService = CreateProductService();
+            var products = productService.GetAllProducts();
+            return Ok(products);
+        }
+
+        public IHttpActionResult GetByTitle(string title)
+        {
+            ProductService productService = CreateProductService();
+            var product = productService.GetTitlePartial(title);
+            return Ok(product);
+        }
+
+        public IHttpActionResult GetById(int id)
+        {
+            ProductService productService = CreateProductService();
+            var product = productService.GetProductById(id);
+            return Ok(product);
+        }
+
+        public IHttpActionResult Post(ProductCreate product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateProductService();
+
+            if (!service.CreateProduct(product))
+                return InternalServerError();
+
+            return Ok("Product successfully created.");
+        }
+
+        public IHttpActionResult Put(ProductEdit product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateProductService();
+
+            if (!service.UpdateProduct(product))
+                return InternalServerError();
+
+            return Ok("Product successfully edited.");
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateProductService();
+
+            if (!service.DeleteProduct(id))
+                return InternalServerError();
+
+            return Ok("Product successfully deleted.");
+        }
+
 
         private ProductService CreateProductService()
         {
