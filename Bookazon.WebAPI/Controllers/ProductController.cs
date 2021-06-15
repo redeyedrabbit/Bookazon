0fe05382e3d9bb1097d8e0a6aec3e98c5494c8bf
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-14 20:43:30 -0400

    Completed the products documentation

--- Bookazon.WebAPI/Controllers/ProductController.cs
+++ Bookazon.WebAPI/Controllers/ProductController.cs
@@ -12,7 +12,7 @@ using System.Web.Http.Description;
 namespace Bookazon.WebAPI.Controllers
 {
     /// <summary>
-    /// 
+    /// Controller for Products and Product Services.
     /// </summary>
     public class ProductController : ApiController
     {
@@ -21,7 +21,7 @@ namespace Bookazon.WebAPI.Controllers
         /// Get all products in the database.
         /// </summary>
         /// <returns>
-        /// The product information for each product in the database, 
+        /// Returns the product information for each product in the database with the ProductId, Title, AuthorId, and TypeOfGenre.
         /// </returns>
         [ResponseType(typeof(ProductListItem))]
         public IHttpActionResult GetAll()
@@ -32,10 +32,13 @@ namespace Bookazon.WebAPI.Controllers
         }
 
         /// <summary>
-        /// Get a product by Title.
+        /// Get a product by partial Title.
         /// </summary>
         /// <param name="title"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// Allows a user to search via a partial title (string) and each item in the database that matches the criteria will be returned with its ProductId, Title, AuthorId, and TypeOfGenre.
+        /// </returns>
+        [ResponseType(typeof(ProductListItem))]
         public IHttpActionResult GetByTitle(string title)
         {
             ProductService productService = CreateProductService();
@@ -44,10 +47,13 @@ namespace Bookazon.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Get a product by its ID.
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// Allows a user to search for a product via that product's ID. Returns the ProductID, Title, AuthorId, and TypeOfGenre.
+        /// </returns>
+        [ResponseType(typeof(ProductListItem))]
         public IHttpActionResult GetById(int id)
         {
             ProductService productService = CreateProductService();
@@ -56,11 +62,14 @@ namespace Bookazon.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Get a range of products within a range of prices.
         /// </summary>
         /// <param name="lowPrice"></param>
         /// <param name="highPrice"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// Allows a user to search for products within a price range by setting the low-end and high-end limits. Returns all products that fit the criteria with their ProductId, Title, AuthorId, and TypeOfGenre.
+        /// </returns>
+        [ResponseType(typeof(ProductListItem))]
         public IHttpActionResult GetByPriceRange(decimal lowPrice, decimal highPrice)
         {
             ProductService productService = CreateProductService();
@@ -69,10 +78,13 @@ namespace Bookazon.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Get all products from a publisher by PublisherId.
         /// </summary>
         /// <param name="publisherId"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// Allows a user to search for products that are tied to one publisher. Returns all product that fit the criteria with their ProductId, Title, AuthorId, and TypeOfGenre.
+        /// </returns>
+        [ResponseType(typeof(ProductListItem))]
         public IHttpActionResult GetByProductsByPublisherId(int publisherId)
         {
             ProductService productService = CreateProductService();
@@ -81,10 +93,13 @@ namespace Bookazon.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Create a new product and add it to the database.
         /// </summary>
         /// <param name="product"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// Allows a user to create a new product. It will require entering in all of the required fields correctly. Before creating a product, be sure to create a Publisher and Author first in their respective tables. If the product is successfully created, returns the message "Product successfully created."
+        /// </returns>
+        [ResponseType(typeof(string))]
         public IHttpActionResult Post(ProductCreate product)
         {
             if (!ModelState.IsValid)
@@ -99,12 +114,16 @@ namespace Bookazon.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Create a new product and add Authorship in one step.
         /// </summary>
         /// <param name="product"></param>
+        /// <param name="authorFirstName"></param>
         /// <param name="authorLastName"></param>
-        /// <returns></returns>
-        public IHttpActionResult PostWithAuthorship(ProductCreate product, string authorLastName)
+        /// <returns>
+        /// Allows a user to create a new product, and add the Authorship through the joining table, in one step. Before creating the product, be sure to create a Publisher and Author first in their respective tables. If the product is successfully created, returns the message "Product and Authorship successfully created."
+        /// </returns>
+        [ResponseType(typeof(string))]
+        public IHttpActionResult PostWithAuthorship(ProductCreate product, string authorFirstName,  string authorLastName)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -114,14 +133,17 @@ namespace Bookazon.WebAPI.Controllers
             if (!service.CreateProductWithAuthor(product, authorLastName))
                 return InternalServerError();
 
-            return Ok("Product and authorship successfully created.");
+            return Ok("Product and Authorship successfully created.");
         }
 
         /// <summary>
-        /// 
+        /// Edit an existing product.
         /// </summary>
         /// <param name="product"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// Allows a user to edit an existing product by that product's ProductId. Each field must be confirmed or changed. Be sure follow the fields in the API to ensure you have all fields edited or remain unchanged. If successful, returns the message "Product successfully edited."
+        /// </returns>
+        [ResponseType(typeof(string))]
         public IHttpActionResult Put(ProductEdit product)
         {
             if (!ModelState.IsValid)
@@ -136,10 +158,13 @@ namespace Bookazon.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Delete an existing product.
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// Allows a user to delete an existing product by that product's ProductId. If successful, returns the message "Product successfully deleted."
+        /// </returns>
+        [ResponseType(typeof(string))]
         public IHttpActionResult Delete(int id)
         {
             var service = CreateProductService();
